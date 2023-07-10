3552719e1d8d9eff4458a6c15a8c3903495405ef
Sathyajith <slakmal68@gmail.com>
2023-07-10 19:51:03 +0530

    added env file

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com',
+  baseURL: process.env.VUE_APP_POST_API_BASE_URL,
 });
 
 export default httpClient;
